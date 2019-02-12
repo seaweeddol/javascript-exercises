8b442ba62cdab4c5834b28f574099ebeda8655df
Jessica Stone <jstone4815@gmail.com>
2019-02-12 12:24:44 -0800

    Complete first test of sumAll to sum two numbers together

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+  let sum = 0;
 
+  for(firstNum; firstNum <= secondNum; firstNum++){
+    sum += firstNum;
+  }
+  return sum;
 }
 
 module.exports = sumAll
