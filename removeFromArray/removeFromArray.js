87894d80a2e6e14d7c43ef68645f9022cc9c80a0
Jessica Stone <jstone4815@gmail.com>
2019-02-12 12:09:32 -0800

    Use Array.from(arguments) and for loop to remove multiple elements from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
 const removeFromArray = function(arr, elementsToRemove) {
-  arr.splice(arr.indexOf(elementsToRemove), 1);
+  let args = Array.from(arguments);
+
+  for(let i = 1; i < args.length; i++){
+    arr.splice(arr.indexOf(args[i]), 1);
+  }
+
   return arr;
 }
 
