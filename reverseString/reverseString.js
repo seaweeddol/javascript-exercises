84e6bc13802c4a5f091f03dd143dbbcd52a3834e
Jessica Stone <jstone4815@gmail.com>
2019-02-11 19:45:43 -0800

    Update reverseString.js to reverse 'hello' to 'olleh' using a loop

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function() {
+  let string = "hello";
+  let stringReverse = "";
 
+  for(let i = string.length - 1; i >= 0; i--){
+    stringReverse += string[i];
+  }
+  return stringReverse;
 }
 
 module.exports = reverseString
