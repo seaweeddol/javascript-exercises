d1fc428eaaa3b02cac89bb65d0b291d7c95398eb
Jessica Stone <jstone4815@gmail.com>
2019-02-12 12:53:21 -0800

    Update leapYears.js to return correct value for any year input

--- leapYears/leapYears.spec.js
+++ leapYears/leapYears.spec.js
@@ -7,16 +7,16 @@ describe('leapYears', function() {
   it('works with non century years', function() {
     expect(leapYears(1997)).toEqual(false);
   });
-  xit('works with ridiculously futuristic non century years', function() {
+  it('works with ridiculously futuristic non century years', function() {
     expect(leapYears(34992)).toEqual(true);
   });
-  xit('works with century years', function() {
+  it('works with century years', function() {
     expect(leapYears(1900)).toEqual(false);
   });
-  xit('works with century years', function() {
+  it('works with century years', function() {
     expect(leapYears(1600)).toEqual(true);
   });
-  xit('works with century years', function() {
+  it('works with century years', function() {
     expect(leapYears(700)).toEqual(false);
   });
 });
