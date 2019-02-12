6d07e1204d9bad2fff5c966dfea697fa70a188ef
Jessica Stone <jstone4815@gmail.com>
2019-02-12 12:19:56 -0800

    Fix removeFromArray.js to ignore non-present values

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -2,7 +2,9 @@ const removeFromArray = function(arr, elementsToRemove) {
   let args = Array.from(arguments);
 
   for(let i = 1; i < args.length; i++){
-    arr.splice(arr.indexOf(args[i]), 1);
+    if(arr.indexOf(args[i]) != -1){
+      arr.splice(arr.indexOf(args[i]), 1);
+    }
   }
 
   return arr;
