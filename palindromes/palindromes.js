78814aa5ecc6e69537e4a419d7e7e1d7fb98e6d5
Jessica Stone <jstone4815@gmail.com>
2019-02-26 19:50:46 -0800

    Fix to work with different cases and punctuation

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function() {
+const palindromes = function(input) {
+  input = input.toLowerCase().replace(/[^\w\s]|_/g, '');
 
+  let first = 0;
+  let last = input.length - 1;
+
+  for(let i = 0; i < input.length; i++) {
+     if (input[first] != input[last]){
+      return false;
+      break;
+    } else {
+      first++;
+      last--;
+      return true;
+    }
+  }
 }
 
 module.exports = palindromes
